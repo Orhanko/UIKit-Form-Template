8b5d4436a4099e1b1926ab0c42884fa4bd2e0fb8
Orhan <95143599+Orhanko@users.noreply.github.com>
2024-12-06 23:53:35 +0100

    Configured centered UIFormComponents in StackView

--- FormViews/ThirdFormMenu/ThirdSubmitViewController.swift
+++ FormViews/ThirdFormMenu/ThirdSubmitViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ThirdSubmitViewController: UIViewController {
 
+    @IBOutlet weak var buttonsContainer: UIView!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var backButton: UIButton!
+    private var isInitialSetupComplete = false
+    private var topSpacer: UIView?
+    private var bottomSpacer: UIView?
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "3/8"
@@ -22,11 +26,17 @@ class ThirdSubmitViewController: UIViewController {
         let proba = CustomTextFieldView(title: "Proba Title 3", description: "Description 3")
         stackView.addArrangedSubview(proba)
         nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        updateSpacerConstraints(topSpacer: topSpacer, bottomSpacer: bottomSpacer, stackView: stackView, buttonsContainer: buttonsContainer, isInitialSetupComplete: isInitialSetupComplete)
+        setupSpacers(stackView: stackView, topSpacer: &topSpacer, bottomSpacer: &bottomSpacer)
     }
     
     @objc func nextButtonTapped() {
         navigateToNextScreen(to: FourthSubmitViewController())
     }
-    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateSpacerConstraints(topSpacer: topSpacer, bottomSpacer: bottomSpacer, stackView: stackView, buttonsContainer: buttonsContainer, isInitialSetupComplete: isInitialSetupComplete)
+        isInitialSetupComplete = true
+    }
     
 }
